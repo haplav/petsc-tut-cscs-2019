5a14f43594152e29c5403723608d62ab3d99dcf4
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2018-05-04 12:12:38 +0200

    added get_petsc_maint.sh get_petsc_master.sh

---
+++ get_petsc_maint.sh
@@ -0,0 +1,3 @@
+# see https://bitbucket.org/petsc/petsc
+
+git clone -b maint https://bitbucket.org/petsc/petsc
---
+++ get_petsc_master.sh
@@ -0,0 +1,3 @@
+# see https://bitbucket.org/petsc/petsc
+
+git clone https://bitbucket.org/petsc/petsc
