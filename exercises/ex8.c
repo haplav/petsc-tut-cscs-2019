70c30d054ee19cfb295685cdf5dcb568695e1a44
Vaclav Hapla <vaclav.hapla@vsb.cz>
2018-05-09 16:27:56 +0200

    ex8 updated according to ex7

--- exercises/ex8.c
+++ exercises/ex8.c
@@ -30,25 +30,40 @@ int main(int argc,char **args)
   */
   ierr = MPI_Allreduce(&n, &N, 1, MPI_INT, MPI_SUM, PETSC_COMM_WORLD);CHKERRQ(ierr);
   ierr = DMDACreate1d(PETSC_COMM_WORLD,DM_BOUNDARY_NONE,N,1,1,NULL,&da);CHKERRQ(ierr);
+  ierr = DMSetFromOptions(da);CHKERRQ(ierr);
+  ierr = DMSetUp(da);CHKERRQ(ierr);
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
                 Create the nonlinear solver and set various options
      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  /*
+     Create nonlinear solver context
+  */
   ierr = SNESCreate(PETSC_COMM_WORLD,&snes);CHKERRQ(ierr);
 
   /*
      Associate SNES with DM.
   */
   ierr = SNESSetDM(snes,da);CHKERRQ(ierr);
+
+  /*
+     Set functions that compute the matrix and RHS.
+  */
   ierr = SNESSetJacobian(snes,NULL,NULL,ComputeJacobian,NULL);CHKERRQ(ierr);
   ierr = SNESSetFunction(snes,NULL,ComputeFunction,NULL);CHKERRQ(ierr);
 
+  /*
+     Set linear and nonlinear solver defaults for this problem (optional).
+  */
   ierr = SNESSetType(snes,SNESNEWTONLS);CHKERRQ(ierr);
   ierr = SNESGetKSP(snes,&ksp);CHKERRQ(ierr);
   ierr = KSPSetType(ksp,KSPCG);CHKERRQ(ierr);
   ierr = KSPGetPC(ksp,&pc);CHKERRQ(ierr);
   ierr = PCSetType(pc,PCNONE);CHKERRQ(ierr);
 
+  /*
+    Set runtime options.
+  */
   ierr = SNESSetFromOptions(snes);CHKERRQ(ierr);
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -57,9 +72,17 @@ int main(int argc,char **args)
   ierr = SNESSolve(snes,NULL,NULL);CHKERRQ(ierr);
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-                      Finalize
+                      Clean-up
      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
   ierr = SNESDestroy(&snes);CHKERRQ(ierr);
+  ierr = DMDestroy(&da);CHKERRQ(ierr);
+
+  /*
+     Always call PetscFinalize() before exiting a program.  This routine
+       - finalizes the PETSc libraries as well as MPI
+       - provides summary and diagnostic information if certain runtime
+         options are chosen (e.g., -log_summary).
+  */
   ierr = PetscFinalize();CHKERRQ(ierr);
   return 0;
 }
@@ -74,20 +97,17 @@ static PetscErrorCode FormMatrix(DM da, Mat A)
   const PetscInt *e;
   PetscInt       row[2];
   PetscScalar    rho;
-  PetscScalar    value[4];
+  PetscScalar    value[4] = {1.0, -1.0, -1.0, 1.0};
 
   PetscFunctionBeginUser;
   ierr = MatGetSize(A,&N,NULL);CHKERRQ(ierr);
 
   ierr = DMDAGetElements(da, &nel, &nen, &e);CHKERRQ(ierr);
- 
-  value[0] = 1.0; value[1] = -1.0; value[2] = -1.0; value[3] = 1.0;
   for (i=0; i<nel; i++) {
     ierr = MatSetValuesLocal(A, 2, e+nen*i, 2, e+nen*i, value, ADD_VALUES);CHKERRQ(ierr);
   }
   ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
   ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
-
   ierr = DMDARestoreElements(da, &nel, &nen, &e);CHKERRQ(ierr);
 
   ierr = MatCreateVecs(A,&d,NULL);CHKERRQ(ierr);
@@ -97,7 +117,6 @@ static PetscErrorCode FormMatrix(DM da, Mat A)
   ierr = VecDestroy(&d);CHKERRQ(ierr);
 
   row[0]=0; row[1]=N-1;
-  //ierr = MatZeroRowsColumns(A,2,row,rho,x,b); CHKERRQ(ierr);
   ierr = MatZeroRowsColumns(A,2,row,rho,NULL,NULL); CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
@@ -107,21 +126,28 @@ static PetscErrorCode FormMatrix(DM da, Mat A)
 static PetscErrorCode FormRHS(DM da, Vec b)
 {
   PetscErrorCode ierr;
-  PetscInt N;
+  PetscInt       i, nel, nen, N;
+  const PetscInt *e;
+  PetscScalar    bvalue[2] = {1.0, 1.0};
+  PetscInt       row[2];
 
   PetscFunctionBeginUser;
   ierr = VecGetSize(b,&N);CHKERRQ(ierr);
-  ierr = VecSet(b,1.0);CHKERRQ(ierr);
 
-  /* take into account Dirichlet b.c. */
-  {
-    PetscInt row[2] = {0, N-1};
-    PetscScalar val[2] = {0.0, 0.0};
-
-    ierr = VecSetValues(b,2,row,val,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecAssemblyBegin(b);CHKERRQ(ierr);
-    ierr = VecAssemblyEnd(b);CHKERRQ(ierr);
+  ierr = DMDAGetElements(da, &nel, &nen, &e);CHKERRQ(ierr);
+  for (i=0; i<nel; i++) {
+    ierr = VecSetValuesLocal(b, 2, e+nen*i, bvalue, ADD_VALUES);CHKERRQ(ierr);
   }
+  ierr = DMDARestoreElements(da, &nel, &nen, &e);CHKERRQ(ierr);
+  ierr = VecAssemblyBegin(b);CHKERRQ(ierr);
+  ierr = VecAssemblyEnd(b);CHKERRQ(ierr);
+
+  /* account for Dirichlet b.c. */
+  bvalue[0] = 0.0; bvalue[1] = 0.0;
+  row[0]=0; row[1]=N-1;
+  ierr = VecSetValues(b,2,row,bvalue,INSERT_VALUES);CHKERRQ(ierr);
+  ierr = VecAssemblyBegin(b);CHKERRQ(ierr);
+  ierr = VecAssemblyEnd(b);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
